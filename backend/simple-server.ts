baff6abf1ca91e602716dd83ca8db400bddbb2c5
eduardonk9999 <thiago.novak7@gmail.com>
2022-10-22 12:11:23 -0300

    create Query armor

--- backend/simple-server.ts
+++ backend/simple-server.ts
@@ -1,17 +1,29 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
+  type Armor {
+    id: String!
+    armor: String!
+  }
+
   type Query {
-    helloWorld: String!
+    armors: [Armor!]!
   }
 `;
+
+interface Armor {
+  id: string
+  name: string
+}
+
+const armors: Armor[] = [];
 
 const server = new ApolloServer({
   typeDefs,
   resolvers: {
    Query: {
-    helloWorld: () => {
-      return 'Hello World'
+    armors: () => {
+      return armors
     }
    }
   },
