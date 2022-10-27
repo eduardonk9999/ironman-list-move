e2f9ead2ce9861c062153b7c0b03e8943d30a69d
eduardonk9999 <thiago.novak7@gmail.com>
2022-10-27 09:03:55 -0300

    comecando a trabalhar os dados

--- backend/simple-server.ts
+++ backend/simple-server.ts
@@ -18,7 +18,8 @@ interface Armor {
 
 const armors: Armor[] = [
   {
-    img: 'https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/f/fb/Iron_Man_Armor_-_Mark_I.png/revision/latest?cb=20181116150929',
+  "https://blogger.googleusercontent.com/img/a/AVvXsEgR5Pslwa5XPlYYCwagpOQObpWgtz-OLYuj1NNSPy3ioQ8gI2gg1vYedWwCz7ofr1Y6FSIb6imWjDd0Ho3win7qT9Mj-lbuqRMgnPssGGTdqTyu1988ePg3vX1kBzib12XhdsHuX1UjFeK9ieDI7dYxQgfV7Ww_79rWasii34bTReEE_bQl8SehHJpJ=w329-h228",
+  img: '',
     armor: 'markOne'
   }
 ];
