3c8661611a7bb12b42e2ff20b3a839899781c307
eduardonk9999 <thiago.novak7@gmail.com>
2022-12-07 14:27:16 -0300

    finalizando json, api list armor

---
+++ backend/irons.ts
@@ -0,0 +1,211 @@
+const armors = [
+
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/f/fb/Iron_Man_Armor_-_Mark_I.png/revision/latest?cb=20181116150929",
+       armor: 'Iron Man Armor 1'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/0/0d/Iron_Man_Armor_-_Mark_II.png/revision/latest?cb=20170728191151",
+       armor: 'Iron Man Armor 2'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/4/47/Iron_Man_Armor_-_Mark_III.png/revision/latest?cb=20170424185316",
+       armor: 'Iron Man Armor 3'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/6/6a/Iron_Man_Armor_-_Mark_IV.png/revision/latest?cb=20180330035146",
+       armor: 'Iron Man Armor 4'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/b/b3/Iron_Man_Armor_-_Mark_V.png/revision/latest?cb=20170424185355",
+       armor: 'Iron Man Armor 5'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/4/4c/Hot_Toys_IM_MK_6_Transparent.png/revision/latest?cb=20170802024240",
+       armor: 'Iron Man Armor 6'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/7/71/Iron_Man_Armor_-_Mark_VII.png/revision/latest?cb=20181207085104",
+       armor: 'Iron Man Armor 7'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/0/0a/Iron_Man_Armor_-_Mark_VIII.jpg/revision/latest?cb=20181207090707",
+       armor: 'Iron Man Armor 8'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/1/1a/Iron_Man_Armor_-_Mark_IX.png/revision/latest?cb=20170801154216",
+       armor: 'Iron Man Armor 9'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/5/5e/Iron_Man_Armor_-_Mark_X.jpg/revision/latest?cb=20181207091221",
+       armor: 'Iron Man Armor 10'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/7/74/Iron_Man_Armor_-_Mark_XI.jpg/revision/latest/scale-to-width-down/1000?cb=20181207091511",
+       armor: 'Iron Man Armor 11'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/e/e4/Iron_Man_Armor_-_Mark_XII.jpg/revision/latest?cb=20181207195940",
+       armor: 'Iron Man Armor 12'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/0/04/Iron_Man_Armor_-_Mark_XIII.jpg/revision/latest/scale-to-width-down/1000?cb=20181207202039",
+       armor: 'Iron Man Armor 13'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/3/3e/Iron_Man_Armor_-_Mark_XIV.jpg/revision/latest?cb=20181207202227",
+       armor: 'Iron Man Armor 14'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/a/aa/Iron_Man_Armor_-_Mark_XV.png/revision/latest?cb=20170424185621",
+       armor: 'Iron Man Armor 15'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/2/2f/Iron_Man_Armor_-_Mark_XVI.jpg/revision/latest?cb=20181207202708",
+       armor: 'Iron Man Armor 16'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/d/db/Iron_Man_Armor_-_Mark_XVII.png/revision/latest?cb=20170424185704",
+       armor: 'Iron Man Armor 17'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/2/2b/Iron_Man_Armor_-_Mark_XVIII.jpg/revision/latest?cb=20181207203242",
+       armor: 'Iron Man Armor 18'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/e/e6/Iron_Man_Armor_-_Mark_XIX.jpg/revision/latest?cb=20181207203830",
+       armor: 'Iron Man Armor 19'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/1/1f/Iron_Man_Armor_-_Mark_XX.png/revision/latest?cb=20170424185724",
+       armor: 'Iron Man Armor 20'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/3/37/Iron_Man_Armor_-_Mark_XXI.png/revision/latest?cb=20170424185744",
+       armor: 'Iron Man Armor 21'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/f/f8/Iron_Man_Armor_-_Mark_XXII.png/revision/latest?cb=20170424185756",
+       armor: 'Iron Man Armor 22'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/6/65/Iron_Man_Armor_-_Mark_XXIII.png/revision/latest?cb=20170801164225",
+       armor: 'Iron Man Armor 23'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/a/a0/Iron_Man_Armor_-_Mark_XXIV.png/revision/latest?cb=20170424185812",
+       armor: 'Iron Man Armor 24'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/8/8c/Iron_Man_Armor_-_Mark_XXV.png/revision/latest?cb=20170424185826",
+       armor: 'Iron Man Armor 25'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/4/4f/Iron_Man_Armor_-_Mark_XXVI.png/revision/latest?cb=20170424185842",
+       armor: 'Iron Man Armor 26'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/b/bb/Iron_Man_Armor_-_Mark_XXVII.png/revision/latest?cb=20170424195610",
+       armor: 'Iron Man Armor 27'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/4/4e/Iron_Man_Armor_-_Mark_XXVIII.jpg/revision/latest?cb=20181207204238",
+       armor: 'Iron Man Armor 28'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/d/db/Iron_Man_Armor_-_Mark_XXIX.jpg/revision/latest/scale-to-width-down/1000?cb=20181207094613",
+       armor: 'Iron Man Armor 29'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/4/4b/Iron_Man_Armor_-_Mark_XXX.png/revision/latest?cb=20170424195652",
+       armor: 'Iron Man Armor 30'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/3/33/Iron_Man_Armor_-_Mark_XXXI.jpg/revision/latest?cb=20181207172820",
+       armor: 'Iron Man Armor 31'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/4/40/Iron_Man_Armor_-_Mark_XXXII.jpg/revision/latest?cb=20180517065231",
+       armor: 'Iron Man Armor 32'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/0/01/Iron_Man_Armor_-_Mark_XXXIII.png/revision/latest?cb=20190407182400",
+       armor: 'Iron Man Armor 33'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/7/78/Iron_Man_Armor_-_Mark_XXXIV.jpg/revision/latest/scale-to-width-down/1000?cb=20181207093725",
+       armor: 'Iron Man Armor 34'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/a/a4/Iron_Man_Armor_-_Mark_XXXV.png/revision/latest?cb=20170424190010",
+       armor: 'Iron Man Armor 35'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/7/7f/Iron_Man_Armor_-_Mark_XXXVI.png/revision/latest?cb=20170424190021",
+       armor: 'Iron Man Armor 36'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/5/5f/Hammerheadengage_2.png/revision/latest/scale-to-width-down/1000?cb=20160105145822",
+       armor: 'Iron Man Armor 37'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/ironman/images/f/f6/Mark_XXXVIII_Igor-IM3SF.png/revision/latest?cb=20140625170038",
+       armor: 'Iron Man Armor 38'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/1/14/Iron_Man_Armor_-_Mark_XXXIX.png/revision/latest?cb=20170424190154",
+       armor: 'Iron Man Armor 39'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/7/71/Iron_Man_Armor_-_Mark_XL.png/revision/latest?cb=20170424190219",
+       armor: 'Iron Man Armor 40' 
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/7/7e/Iron_Man_Armor_-_Mark_XLI.png/revision/latest?cb=20170424190235",
+       armor: 'Iron Man Armor 41'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/3/3b/Iron_Man_Armor_-_Mark_XLII.png/revision/latest?cb=20170424190250",
+       armor: 'Iron Man Armor 42'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/3/3e/Iron_Man_Armor_-_Mark_XLIII.png/revision/latest?cb=20170424190318",
+       armor: 'Iron Man Armor 43'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/0/0b/Iron_Man_Armor_-_Mark_XLIV.png/revision/latest?cb=20181207092727",
+       armor: 'Iron Man Armor 44'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/c/c2/Iron_Man_Armor_-_Mark_XLV.png/revision/latest?cb=20170425073037",
+       armor: 'Iron Man Armor 45'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/7/70/Iron_Man_Armor_-_Mark_XLVI.png/revision/latest?cb=20170424190420",
+       armor: 'Iron Man Armor 46'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/f/f3/Iron_Man_Armor_-_Mark_XLVII.png/revision/latest?cb=20170703052625",
+       armor: 'Iron Man Armor 47'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/5/5f/Iron_Man_Armor_-_Mark_XLVIII.png/revision/latest?cb=20180526010541",
+       armor: 'Iron Man Armor 48'
+    },
+  
+    {  img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/7/7a/Iron_Man_Armor_-_Mark_XLIX.png/revision/latest?cb=20190525040505",
+       armor: 'Iron Man Armor 49'
+    },
+  
+    {
+       img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/a/a0/Iron_Man_Armor_-_Mark_L.png/revision/latest?cb=20180318221316",
+       armor: 'Iron Man Armor 50'
+    },
+  
+    {
+      img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/f/f2/Iron_Man_Armor_-_Mark_LXXXV.png/revision/latest?cb=20190401222437",
+      armor: 'Iron Man Armor 51'
+    }
+
+]
+
+export default armors
--- backend/simple-server.ts
+++ backend/simple-server.ts
@@ -1,13 +1,16 @@
 import { ApolloServer, gql } from "apollo-server";
+import armors from "./irons";
+
+
 
 const typeDefs = gql`
   type Armor {
-    img: String!
-    armor: String!
+    img: String
+    armor: String
   }
 
   type Query {
-    armors: [Armor!]!
+    armors: [Armor]
   }
 `;
 
@@ -16,13 +19,8 @@ interface Armor {
   armor: string
 }
 
-const armors: Armor[] = [
-  {
-  "https://blogger.googleusercontent.com/img/a/AVvXsEgR5Pslwa5XPlYYCwagpOQObpWgtz-OLYuj1NNSPy3ioQ8gI2gg1vYedWwCz7ofr1Y6FSIb6imWjDd0Ho3win7qT9Mj-lbuqRMgnPssGGTdqTyu1988ePg3vX1kBzib12XhdsHuX1UjFeK9ieDI7dYxQgfV7Ww_79rWasii34bTReEE_bQl8SehHJpJ=w329-h228",
-  img: '',
-    armor: 'markOne'
-  }
-];
+
+
 
 const server = new ApolloServer({
   typeDefs,
